63f68cdbb967633284285aa6381c3fb2e1a091d3
Tasikarmy <55316231+Tasikarmy@users.noreply.github.com>
2019-09-14 17:51:16 +0700

    Add files via upload

---
+++ apwjejrbsjdjdkmfnaheiiwowoajdhalahdhqhahaixbakdoaudkalalhfj.py
@@ -0,0 +1,14 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('Menginstall Tols.....')
+clear
+mengetik('Wait......')
+clear
+mengetik('subscribe')
+mengetik('RANDI OLOYY')
